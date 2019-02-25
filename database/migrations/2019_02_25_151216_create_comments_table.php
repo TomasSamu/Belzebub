2950627307d204868d93766a16a636c1ec182591
TPolan <t.poly@seznam.cz>
2019-02-25 16:21:07 +0100

    created models: Location, Comment

---
+++ database/migrations/2019_02_25_151216_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('text');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('event_id')->nullable();
+            $table->unsignedInteger('offer_id')->nullable();
+            $table->unsignedInteger('comment_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
