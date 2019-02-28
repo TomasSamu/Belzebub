4083fe9fc761f73e79481de4684e0bb23b5a0b87
NushySC <ank.sampedro@gmail.com>
2019-02-28 21:57:23 +0100

    trying main layout

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,11 +1,25 @@
-<div class="right">
-    <li>One thing</li>
-    <li>Another thing</li>
-    <li>Third thing</li>
-</div>
 
-<div class="left">
-    <li>One left thing</li>
-    <li>Another left thing</li>
-    <li>Last third thing</li>
-</div>
+    <div class="container-fluid bg-dark text-center text-white p-3">
+            <h3>Footer</h3>
+        <div class="row">
+            
+            <div class="col-sm-6 text-center">
+              <div class="card">
+                <div class="card-body bg-dark text-white">
+                    <li>One thing</li>
+                    <li>Another thing</li>
+                    <li>Third thing</li>
+                </div>
+              </div>
+            </div>
+    
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body text-center bg-dark text-white">
+                    <li>One left thing</li>
+                    <li>Another left thing</li>
+                    <li>Last third thing</li>
+                </div>
+              </div>
+            </div>
+          </div>
