c8aada02faa4197ec1ef1abe8088e5715592aa69
TPolan <t.poly@seznam.cz>
2019-03-07 10:43:20 +0100

    making the pivot table seeds

--- database/seeds/PivotSeeder.php
+++ database/seeds/PivotSeeder.php
@@ -20,8 +20,35 @@ class PivotSeeder extends Seeder
             $user->boardgames()->attach([(rand(1,$boardgames->count())),(rand(1,$boardgames->count())),(rand(1,$boardgames->count())),(rand(1,$boardgames->count())),(rand(1,$boardgames->count())),(rand(1,$boardgames->count()))]);
         }
 
-        // $genre = Genre::find(1);
-        // $boardgames = Boardgame::where('id',49);
-        // $genre->boardgames()->attach([$boardgames->id]);
+        $genre = Genre::find(1);
+        $boardgames = Boardgame::whereIn('id',[49])->get();
+        foreach ($boardgames as $boardgame) {
+        $genre->boardgames()->attach([$boardgame->id]);
+        }
+        $genre = Genre::find(2);
+        $boardgames = Boardgame::whereIn('id',[1,12,16,18,19,23,30,35,37,41,43])->get();
+        foreach ($boardgames as $boardgame) {
+        $genre->boardgames()->attach([$boardgame->id]);
+        }
+        $genre = Genre::find(3);
+        $boardgames = Boardgame::whereIn('id',[10])->get();
+        foreach ($boardgames as $boardgame) {
+        $genre->boardgames()->attach([$boardgame->id]);
+        }
+        $genre = Genre::find(5);
+        $boardgames = Boardgame::whereIn('id',[9,24,32,40])->get();
+        foreach ($boardgames as $boardgame) {
+        $genre->boardgames()->attach([$boardgame->id]);
+        }
+        $genre = Genre::find(6);
+        $boardgames = Boardgame::whereIn('id',[13,15])->get();
+        foreach ($boardgames as $boardgame) {
+        $genre->boardgames()->attach([$boardgame->id]);
+        }
+        $genre = Genre::find(7);
+        $boardgames = Boardgame::whereIn('id',[47])->get();
+        foreach ($boardgames as $boardgame) {
+        $genre->boardgames()->attach([$boardgame->id]);
+        }
     }
 }
