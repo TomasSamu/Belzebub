9e3f1c89539fab6815fc0980ef504a59cdfcc595
TPolan <t.poly@seznam.cz>
2019-03-22 15:33:40 +0100

    location seeder done

--- database/seeds/EventSeeder.php
+++ database/seeds/EventSeeder.php
@@ -13,20 +13,16 @@ class EventSeeder extends Seeder
     public function run()
     {
         $events = [];
-        for ($i;$i === 50; $i++) {
-            $events +=  [
+        for ($i = 1;$i < 51; $i++) {
+            Event::create( [
                 "user_id" => rand(1,6),
                 "location_id" => rand(1,7),
                 "title" => "Dummy Event n." . $i,
                 "text" => "Regular Boardgame session",
                 "time" => "19:00",
-                "date" => "2019-" . rand(0,28) . "-" . rand(1,12),
+                "date" => "2019-04-" . rand(10,28),
                 "num_of_players" => rand(1,6)
-            ];
-        }
-        foreach($events as $event)
-        {
-            Event::create($event);
+            ]);
         }
     }
 }
