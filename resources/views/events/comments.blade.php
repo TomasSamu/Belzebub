a328f14b4349a870eadda0f99c0ffb48abb9d900
NushySC <ank.sampedro@gmail.com>
2019-03-22 14:07:01 +0100

    comments haflfdome, btn attend added

--- resources/views/events/comments.blade.php
+++ resources/views/events/comments.blade.php
@@ -1,41 +1,44 @@
+<div class="comment">
+    <div class="comment-head">
+        <div class="user-avatar">
+            <i class="fas fa-comment"></i></div>
+        <div class="user-name">{{ $comment->user->name }}</div>
+        <div class="date-posted"> Posted at: {{$comment->created_at}}</div>
+    </div>
+    <div class="comment-right">
+        <div class="comment-text">{{ $comment->text }}    </div>
+
+    <button for="text"class="btn btn-xs btn-green">Reply:</button>
+</div>
+
+<div class="show-reply">
+        @auth
+        <form action="{{action('CommentController@store', $event->id)}}" method="post">
+            @csrf
+
+            <div class="form-group reply" id="comment-reply">
+
+                <input type="hidden" name="comment_id" value={{$comment->id}}>
+                <textarea name="text" id="comment" cols="50" rows="3"></textarea>
+                <button type="submit" value="submit comment" class="btn btn-xs btn-amber">Submit</button>
+            </div>        
+        </form>
+        @endauth
+    </div>
+
+</div>
 
 
-                <div class="comment">   
-                        <div class="comment-left">
-                            <div class="user-avatar">
-        {{--                          <img class="img-fluid"
-                                    src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/user-male-icon.png"/>
-        --}}                     </div>
-                            <div class="user-name">{{ $comment->user->name }}</div>
-                        </div>
-                        <div class="comment-right">
-                            {{ $comment->text }}
-                        </div>
-    
-                    </div>
-                    
-                    <div class="comments-reply" style="padding-left: 50px;">
-                        @auth
-                            <form action="{{action('CommentController@store', $event->id)}}" method="post">
-                            @csrf
-                                <div class="form-group">
-                                    <label for="text">Reply:</label><br>
-                                    <input type="hidden" name="comment_id" value={{$comment->id}}>
-                                    <textarea name="text" id="comment" cols="50" rows="5"></textarea>
-                                </div>
-                            
-                                <div class="form-group">
-                                    <input type="submit" value="submit comment">
-                                </div>
-                            </form>
-                        @endauth
-    
-                        @if($comment->replies()->count() > 0)
-    
-    
-                            @foreach ($comment->replies as $comment)
-                                @include('events.comments')
-                            @endforeach
-                        @endif
-    
-                    </div>
+<div class="comments-reply">
+
+
+
+    @if($comment->replies()->count() > 0)
+
+
+    @foreach ($comment->replies as $comment)
+    @include('events.comments')
+    @endforeach
+    @endif
+
+</div>
