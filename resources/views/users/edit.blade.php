72b4e21db17e19efab6dd0758628fb1d0983d70b
TPolan <t.poly@seznam.cz>
2019-03-25 14:59:47 +0100

    fixed user edit

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -14,33 +14,33 @@
         @method('PUT')
         @csrf
         <div class="form-group">
-            <label for="formGroupExampleInput">Title</label>
+            <label for="formGroupExampleInput">Name</label>
             <input type="text" class="form-control" name="name" id="name" value="{{$user->name}}">
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput2">Publishing year</label>
+            <label for="formGroupExampleInput2">Username</label>
             <input type="text" class="form-control" id="year" name="username" value="{{$user->username}}">
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput2">Minimal number of Players</label>
+            <label for="formGroupExampleInput2">Email</label>
             <input type="text" class="form-control" id="min_players" name="email" value="{{$user->email}}">
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput2">Maximal number of Players</label>
+            <label for="formGroupExampleInput2">Year of Birth</label>
             <input type="text" class="form-control" id="max_players" name="year_of_birth" value="{{$user->year_of_birth}}">
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput2">Recommended Age</label>
+            <label for="formGroupExampleInput2">Gender</label>
             <input type="text" class="form-control" id="age_range" name="age_range" value="{{$user->gender}}">
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput2">Short description of the user</label>
+            <label for="formGroupExampleInput2">City</label>
             <input type="text" class="form-control" id="description" name="city" value="{{$user->city}}">
         </div>
-        <div class="form-group">
+        {{-- <div class="form-group">
             <label for="formGroupExampleInput2">Average play time</label>
             <input type="text" class="form-control" id="play_time" name="image" value="{{$user->image}}">
-        </div>
+        </div> --}}
         <div class="form-group">
             <input type="submit" class="btn btn-sm btn-amber" value="Update">
         </div>
