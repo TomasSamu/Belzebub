93b4c8a713f5c31cb9c12c9a28024e871c7327af
NushySC <ank.sampedro@gmail.com>
2019-03-26 16:11:19 +0100

    random stuf including pagination

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -7,48 +7,51 @@
         @csrf
         <input type="submit" value="Add a new location" class="btn btn-sm btn-amber"></form>
         <h2 class="title-bar">Locations</h2>
-    <div class="pagination pg-amber">{{$locations->onEachSide(1)->links()}}
-    </div>
+
 </div>
 
-<div class="grid-container">
-
-    @foreach ($locations as $location)
-    <div class="card">
-        <div class="view overlay">
-
-            <img class="card-img-top" src="{{$location->image}}" alt="Card image cap" />
-            <div class="mask rgba-indigo-strong">
+<div class="container">
+    <div class="grid-container">
+    
+        @foreach ($locations as $location)
+        <div class="card">
+            <div class="view overlay">
+    
+                <img class="card-img-top" src="{{$location->image}}" alt="Card image cap" />
+                <div class="mask rgba-indigo-strong">
+                </div>
+            </div>
+    
+            <div class="card-body">
+                <h4 class="card-title">{{$location->name}}</h4>
+                <p>Average Rating: {{round($location->ratings()->avg('rating'), 2)}} </p>
+    
+                <div class="buttons-edit">
+                    <form method="GET" action="{{action('LocationController@show',$location->id)}}">
+                        @csrf
+                        <button type="submit" value="Detail" class="btn btn-icon btn-blue"><i
+                                class="fas fa-info"></i></button>
+                    </form>
+                    @can('admin')
+                    <form method="POST" action="{{action('LocationController@destroy',$location->id)}}">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" value="Delete" class="btn btn-icon btn-red"><i
+                                class="fas fa-trash"></i></button></form>
+                    <form method="GET" action="{{action('LocationController@edit',$location->id)}}">
+                        @csrf
+                        <button type="submit" value="Edit" class="btn btn-icon btn-info"><i class="fas fa-pen"></i></button>
+                    </form>
+                    @endcan
+                </div>
             </div>
         </div>
-
-        <div class="card-body">
-            <h4 class="card-title">{{$location->name}}</h4>
-            <p>Average Rating: {{round($location->ratings()->avg('rating'), 2)}} </p>
-
-            <div class="buttons-edit">
-                <form method="GET" action="{{action('LocationController@show',$location->id)}}">
-                    @csrf
-                    <button type="submit" value="Detail" class="btn btn-icon btn-blue"><i
-                            class="fas fa-info"></i></button>
-                </form>
-                @can('admin')
-                <form method="POST" action="{{action('LocationController@destroy',$location->id)}}">
-                    @method('DELETE')
-                    @csrf
-                    <button type="submit" value="Delete" class="btn btn-icon btn-red"><i
-                            class="fas fa-trash"></i></button></form>
-                <form method="GET" action="{{action('LocationController@edit',$location->id)}}">
-                    @csrf
-                    <button type="submit" value="Edit" class="btn btn-icon btn-info"><i class="fas fa-pen"></i></button>
-                </form>
-                @endcan
-            </div>
-        </div>
+        @endforeach
+    
+    
     </div>
-    @endforeach
-
-
+    <div class="pagination pg-amber">{{$locations->onEachSide(1)->links()}}
+        </div>
 </div>
 
 @endsection
